e4a37bbc46323bc51f4bf34f3c8bbeed9bee0136
Tsclever <yxt15850005248@outlook.com>
2024-07-14 16:19:02 +0800

    正则表达式和re模块的使用

--- demo5.py
+++ demo5.py
@@ -54,44 +54,61 @@
 import re
 
 # findall(): 字符串中所有的符合正则的内容（效率不高，网页内容很多很长）
-# \d 匹配数字   + 重复 一次 或 更多次   \d+ 匹配里面的所有数字
+list1 = re.findall("m", "maybe you are a mm, mai yi mai")
+print(list1)   # ['m', 'm', 'm', 'm', 'm']
+
+# \d 匹配数字   + 重复一次或更多次   \d+ 匹配里面的所有数字
 list1 = re.findall(r"\d+", "我的电话是：10086，我女盆友的电话是：10010")
-# 输出 ['10086', '10010']
-print(list1)
+print(list1)   # ['10086', '10010']
+
 
 # finditer: 匹配字符串中所有的内容(返回的是迭代器), 从迭代器中拿到内容需要 .group() 方法
-list2 = re.finditer(r"\d+", "我的电话是：10086，我女盆友的电话是：10010")
-# 输出 <callable_iterator object at 0x7fd9c533bb50>   这里说明了list2是 迭代器
-# print(list2)
+list2 = re.finditer(r"\d+", "我的电话是：10086")
+# print(list2)   <callable_iterator object at 0x7fd9c533bb50>   这里说明了list2是 迭代器
+
 for i in list2:
-# 输出 <re.Match object; span=(6, 11), match='10086'>   正则表达式; 字符串从索引 6 开始，到索引 11 结束, 匹配到的字符串是 10086
-#     <re.Match object; span=(21, 26), match='10010'> 
-# print(i)
+# print(i)   <re.Match object; span=(6, 11), match='10086'>   正则表达式; 字符串从索引 6 开始，到索引 11 结束, 匹配到的字符串是 10086
+  print(i.group())   # 10086
 
-# 输出 10086
-#      10010
-  print(i.group())
 
-# search, 找到一个结果就返回, 返回的结果是match对象, 拿数据需要 .group()
+# search, 找到一个结果就返回, 返回的结果是match对象, 如果匹配不上，search返回的结果是none, 拿数据需要 .group()
 list3 = re.search(r"\d+", "我的电话是：10086，我女盆友的电话是：10010")
-# 输出10086
-print(list3.group())
+print(list3.group())   # 输出10086
 
-# match 是从头开始匹配, 条件是 \d+ , 只匹配纯 数字, 类似于 ^\d+
-# list4 = re.match(r"\d+", "我的电话是：10086，我女盆友的电话是：10010")
-# 'NoneType' object has no attribute 'group'   list4 为空
-# print(list4.group())
+
+# match 是从头开始匹配, 条件是 \d+ , 只匹配纯数字, 类似于 ^\d+
+list4 = re.match(r"\d+", "我的电话是：10086，我女盆友的电话是：10010")
+print(list4.group())   # 'NoneType' object has no attribute 'group'   list4 为空
 
 list4 = re.match(r"\d+", "10086，我女盆友的电话是：10010")
-# 输出10086
-print(list4.group())
+print(list4.group())   # 10086
+
 
 # 预加载正则表达式     提前加载，提高效率
-obj = re.compile(r"\d+")
+obj = re.compile(r"\d+")   # 将正则表达式编译成一个 正则表达式对象，规则为匹配所有数字
 
-list5 = obj.finditer("我的电话是：10086，我女盆友的电话是：10010")
+list5 = obj.finditer("我的电话是: 10086")   # 正则表达式调用 finditer
 for i in list5:
-  print(i.group())
+  print(i.group())   #10086
 
-list6 = obj.findall("么么哒，我就不信你不换我1001001001")
-print(list6)
+list5 = obj.findall("替换成: 1001001001")   # 正则表达式调用 findall
+for i in list5:
+  print(i)   # 1001001001
+
+
+s = """""
+<div class='zjl'><span id='1'>周杰伦</span></div>
+<div class='jjj'><span id='2'>林俊杰</span></div>
+<div class='xzq'><span id='3'>薛之谦</span></div>
+<div class='cyl'><span id='4'>蔡依林</span></div>
+<div class='jjs'><span id='5'>界沮授</span></div>
+"""
+
+# (?P<分组名字>正则) 可以单独从正则匹配的内容中进一步提取内容
+obj = re.compile(r"<div class='.*?'><span id='(?P<id>\d+)'>(?P<name>.*?)</span></div>", re.S)  # re.S  让 . 能匹配换行符
+
+result = obj.finditer(s)
+for i in result:
+  print(i.group())        # <div class='zjl'><span id='1'>周杰伦</span></div>
+  print(i.group("id"))    # 获取id组的内容，输出：1
+  print(i.group("name"))  # 获取name组的内容，输出：周杰伦
