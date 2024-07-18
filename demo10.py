c08250bf7312cfa6b5babb82e98a6e2f9cbca416
Tsclever <yxt15850005248@outlook.com>
2024-07-18 19:18:20 +0800

    爬取优美图库

--- demo10.py
+++ demo10.py
@@ -20,21 +20,23 @@ alist = main_page.find("div", {"class":"TypeList"}).find_all("a")
 
 for a in alist:
   href = a.get("href")   # 直接通过get就可以拿到属性的值
+
   # 拿到子页面的源代码
-  chid_page_resp = requests.get(href)
-  chid_page_resp.encoding = "utf-8"
-  chid_page_resp_text = chid_page_resp.text
+  chid_resp = requests.get(href)
+  chid_resp.encoding = "utf-8"
+
   # 从子页面中拿到图片的下载路径
-  chid_page = BeautifulSoup(chid_page_resp_text, "html.parser")
+  chid_page = BeautifulSoup(chid_resp.text, "html.parser")
   p = chid_page.find("p", align="center")
   img = p.find("img")
-  src = img.get("src")
+  src = img.get("src")   # 这里需要注意一下, .get 不能直接再bs对象上操作
+
   # 下载图片
-  img_resp = requests.get(src)
-  # img_resp.content   # 这里拿到的是字节
+  img_resp = requests.get(src)    # 先发送请求, 再切割链接作为名字, 这里容易搞混
   img_name = src.split("/")[-1]   # 通过 "/" 来切割, 并且取到最后一个内容
-  with open("img/"+img_name, "wb") as file:
-    file.write(img_resp.content)
+  with open("img/"+img_name, "wb") as file:   # 先用切割好的名字创建文件
+    # img_resp.content   # 这里拿到的是字节       
+    file.write(img_resp.content)              # 再用从请求中获得的二进制写入到文件中, 这里也容易搞混
 
   print("over!", img_name)
   time.sleep(1) # 设置1秒延迟
