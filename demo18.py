64cd855963065d633348f31673e14d53b7b66f20
Tsclever <yxt15850005248@outlook.com>
2024-07-23 15:19:01 +0800

    多线程和多进程

--- demo18.py
+++ demo18.py
@@ -1,10 +1,11 @@
-# 项目18 多线程
+# 项目18 多线程和多进程
 # 项目介绍: 线程, 进程
 #          进程是资源单位, 每一个进程至少要有一个线程
 #          线程是执行单位
 
 # 启动每一个程序默认都会有一个主线程
 from threading import Thread   # 线程类模块
+from multiprocessing import Process   # 进程模块
 
 # 单进程
 
@@ -35,19 +36,46 @@ from threading import Thread   # 线程类模块
 #     print("我是主路线", i)
 
 
-# 方法二
-# 大佬爱使用
-class MyThread(Thread):   # 继承了Thread就是子类, Thread的特性是线程, 那么子类也会有线程的特性
-  def run(self):   # 固定的  ->  当线程被执行的时候, 被执行的就是run()
-    for i in range(100):
-      print("子线程", i)
+# 给线程起名
+def func(name):
+  for i in range(500):
+    print(name, i)
 
 if __name__ == "__main__":
-  t = MyThread()
-  # t.run()   # 方法的调用  ->  单线程?
-  t.start()   # 开启线程
+  t1 = Thread(target=func, args=("周杰伦",))   # 传递参数必须是元组
+  t1.start()
 
-  for i in range(100):
-    print("主路线", i)
+  t2 = Thread(target=func, args=("王力宏",))
+  t2.start()
+
+# args = ("王力宏")   # 这是一个字符串，不是元组
+# args = ("王力宏",)  # 这是一个包含一个元素的元组
 
 
+# 方法二
+# 大佬爱使用
+# class MyThread(Thread):   # 继承了Thread就是子类, Thread的特性是线程, 那么子类也会有线程的特性
+#   def run(self):   # 固定的  ->  当线程被执行的时候, 被执行的就是run()
+#     for i in range(100):
+#       print("子线程", i)
+
+# if __name__ == "__main__":
+#   t = MyThread()
+#   # t.run()   # 方法的调用  ->  单线程?
+#   t.start()   # 开启线程
+
+#   for i in range(100):
+#     print("主路线", i)
+
+
+# 多进程
+# def func():
+#   for i in range(500):
+#     print("子进程", i)
+
+# if __name__ == "__main__":
+#   p = Process(target=func)
+#   p.start()
+
+#   for i in range(500):
+#     print("我是主进程", i)
