310f1e0f26b4de3550e30a13a294f905537b96dc
tsclever <tsclever@outlook.com>
2025-02-27 13:39:49 +0800

    09_爬取农产品市场行情

--- 09_爬取农产品市场行情.py
+++ 09_爬取农产品市场行情.py
@@ -5,68 +5,68 @@
 #          4.存入到data.csv文件中
 #注意: 这一个项目涉及到了bs4库的使用, 需要我们多加练习。
 
-# import requests
-# from bs4 import BeautifulSoup
-# import csv
+import requests
+from bs4 import BeautifulSoup
+import csv
 
-# url = "http://sc.cqnync.cn/marketSta/?vexp=3&classId=3"
-# resp = requests.get(url)
+url = "http://sc.cqnync.cn/marketSta/?vexp=3&classId=3"
+resp = requests.get(url)
 
-# with open("data.csv", "w", encoding="utf-8") as file:
-#   # 声明csv
-#   csv_writer = csv.writer(file)
-#   csv_writer.writerow(["name","location","year","money","kid"])
+with open("data.csv", "w", encoding="utf-8") as file:
+  # 声明csv
+  csv_writer = csv.writer(file)
+  csv_writer.writerow(["name","location","year","money","kid"])
 
-#   # 解析数据
-#   # 把页面源代码交给BeautifulSoup进行处理, 生成BeautifulSoup对象
-#   page = BeautifulSoup(resp.text, "html.parser")   # 声明resp.text 是 html
-#   # 从bs对象中查找数据
-#   # find(标签, 属性=值) 只找一个
-#   # find_all(标签, 属性=值) 找全部
+  # 解析数据
+  # 把页面源代码交给BeautifulSoup进行处理, 生成BeautifulSoup对象
+  page = BeautifulSoup(resp.text, "html.parser")   # 声明resp.text 是 html
+  # 从bs对象中查找数据
+  # find(标签, 属性=值) 只找一个
+  # find_all(标签, 属性=值) 找全部
 
-#   # table = page.find("table", class_="list")   # class是Python的关键字, class_代表的是html里的class
-#   table = page.find("table", attrs={"class":"list"})   # 等同于上一行, 可以避免使用class_
-#   # 拿到所有数据行
-#   trs = table.find_all("tr")[1:]
-#   for tr in trs:   # 每一行
-#     tds = tr.find_all("td")
-#     name = tds[0].text    # .text 表示拿到被标签标记的内容 <td>内容</td>
-#     loc = tds[1].text     # .text 表示拿到被标签标记的内容 <td>内容</td>
-#     year = tds[2].text    # .text 表示拿到被标签标记的内容 <td>内容</td>
-#     money = tds[4].text   # .text 表示拿到被标签标记的内容 <td>内容</td>
-#     kid = tds[6].text     # .text 表示拿到被标签标记的内容 <td>内容</td>
+  # table = page.find("table", class_="list")   # class是Python的关键字, class_代表的是html里的class
+  table = page.find("table", attrs={"class":"list"})   # 等同于上一行, 可以避免使用class_
+  # 拿到所有数据行
+  trs = table.find_all("tr")[1:]
+  for tr in trs:   # 每一行
+    tds = tr.find_all("td")
+    name = tds[0].text    # .text 表示拿到被标签标记的内容 <td>内容</td>
+    loc = tds[1].text     # .text 表示拿到被标签标记的内容 <td>内容</td>
+    year = tds[2].text    # .text 表示拿到被标签标记的内容 <td>内容</td>
+    money = tds[4].text   # .text 表示拿到被标签标记的内容 <td>内容</td>
+    kid = tds[6].text     # .text 表示拿到被标签标记的内容 <td>内容</td>
 
-#     csv_writer.writerow([name,loc,year,money,kid])
-# print("over!")
+    csv_writer.writerow([name,loc,year,money,kid])
+print("over!")
 
 
 # 学成归来！
 # 攻克 北京新发地
 # 网站地址：http://www.xinfadi.com.cn/index.html
-import requests
+# import requests
 
-url = "http://www.xinfadi.com.cn/getCat.html"
+# url = "http://www.xinfadi.com.cn/getCat.html"
 
-print("请选择对应编号：蔬菜:1186 水果:1187 肉禽蛋:1188 水产:1189 粮油:1190 豆制品:1203 调料:1204")
-choice = input("请输入编号：")
+# print("请选择对应编号：蔬菜:1186 水果:1187 肉禽蛋:1188 水产:1189 粮油:1190 豆制品:1203 调料:1204")
+# choice = input("请输入编号：")
 
-headers = {
-  "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
-}
+# headers = {
+#   "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+# }
 
-parms = {
-  "prodCatid" : choice
-}
+# parms = {
+#   "prodCatid" : choice
+# }
 
-resp = requests.post(url, headers=headers, params=parms)
-data = resp.json()
+# resp = requests.post(url, headers=headers, params=parms)
+# data = resp.json()
 
-for i in data['list']:
-  print("品名：", i['prodName'])
-  print("最低价：", i['lowPrice'])
-  print("平均价：", i['avgPrice'])
-  print("最高价：", i['highPrice'])
-  print("产地：", i['place'])
-  print("单位：", i['unitInfo'])
-  print("发布日期：", i['pubDate'])
-  print("========================================================")
+# for i in data['list']:
+#   print("品名：", i['prodName'])
+#   print("最低价：", i['lowPrice'])
+#   print("平均价：", i['avgPrice'])
+#   print("最高价：", i['highPrice'])
+#   print("产地：", i['place'])
+#   print("单位：", i['unitInfo'])
+#   print("发布日期：", i['pubDate'])
+#   print("========================================================")
